776627e683f8364efe435d3505856a9af1f98728
Anyta Ali <anyta.ali86@gmail.com>
2018-01-09 13:00:35 -0600

    push

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,4 @@
-// (function(){
+// This Js File is for the LoginPage only. 
 $(document).ready(function() {
 //Initialize Firebase 
   var config = {
@@ -12,57 +12,88 @@ $(document).ready(function() {
   firebase.initializeApp(config);
 
   //Get Elements 
+  var txtName = $('#txtName'); 
   var txtEmail = document.getElementById('txtEmail');
-  var txtPassword = document.getElementById('txtPassword'); 
+  var txtPassword = document.getElementById('txtPassword');
+  if (txtEmail.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (txtPassword.length < 4) {
+          alert('Please enter a password.');
+          return;
+        } 
   var btnLogin = document.getElementById('btnLogin'); 
   var btnSignUp = document.getElementById('btnSignUp'); 
   var btnLogout = document.getElementById('btnLogout');
+//Hidden Name: 
+$('.registerName').hide();
+$('#btnSignUp').hide(); 
 
+
+//hanldes Registration Button
+
+$('#btnRegiter').on('click', function(){
+  event.preventDefault(); 
+  $('.registerName').show();
+  $('#btnSignUp').show(); 
+}); 
+
+  //Handles Login/sign
   //add login event on click listener
   btnLogin.addEventListener('click', e=>{
     event.preventDefault(); 
-    alert("click");
-  	//Get email and pass 
-  	var email = txtEmail.value; 
-  	var pass = txtPassword.value; 
-  	var auth = firebase.auth(); 
-  	// sign in 
-  	var promise = auth.signInWithEmailAndPassword(email, pass); 
-  	promise.catch(e => console.log(e.message)); 
-    console.log("hello"); 
+    //Get email and pass 
+    var email = txtEmail.value; 
+    var pass = txtPassword.value; 
+    var auth = firebase.auth(); 
+    // sign in 
+    var promise = auth.signInWithEmailAndPassword(email, pass); 
+    promise.catch(e => alert(e.message)); 
+    console.log("Logging in");
   }); 
 
-
-  //add signUp Event
+//Hanldes SignUP
   btnSignUp.addEventListener('click', e=>{
-  	//Get email and pass
+    //Get email and pass
     //TO DO: check for real emails 
     event.preventDefault(); 
-    alert("clicked"); 
-  	var email = txtEmail.value; 
-  	var pass = txtPassword.value; 
-  	var auth = firebase.auth(); 
-  	// sign in 
-  	var promise = auth.createUserWithEmailAndPassword(email, pass); 
-  	promise.catch(e => console.log(e.message)); 
-  }); 
+    // alert("You've Signed up"); 
+    var email = txtEmail.value; 
+    var pass = txtPassword.value; 
+    var auth = firebase.auth(); 
+    // sign in 
+    var promise = auth.createUserWithEmailAndPassword(email, pass); 
+    promise.catch(e => console.log(e.message));
+  });  
 
-  btnLogout.addEventListener('click', e=>{
-    firebase.auth().signOut(); 
-    console.log('clicked')
-  }); 
+// //Hanldes SignOut / Log Out 
+//   btnLogout.addEventListener('click', e=>{
+//     firebase.auth().signOut(); 
+//     console.log('Users have logged Out');  
+//   }); 
 
-  //add a realtime Listener
-  firebase.auth().onAuthStateChanged(firebaseUser=> { 
-  	if(firebaseUser) {
+//add a realtime Listener /handles Realtime Logged in User
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    event.preventDefault(); 
+    location.href = "landingpage.html"
+    console.log(user); 
+    console.log(email); 
+    console.log(firebase.user + 'user'); 
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
 
-  		console.log(firebaseUser);
-      location.href = "landingpage.html" 
-  	} else{
-  		console.log('not logged in'); 
-  	}
-  }); 
-
-}); 
-
-// }()); 
+});   
